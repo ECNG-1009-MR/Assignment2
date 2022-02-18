6ad2b6972074284230093fefd9a00aa5ec93fd21
Matthias Elliot <matthias.elliot@my.uwi.edu>
2022-02-17 21:46:16 -0400

    Sections allocated for each part. Comments should be deleted when we're finished.

--- Assignment2/Assignment2.cpp
+++ Assignment2/Assignment2.cpp
@@ -1,7 +1,45 @@
 // Assignment2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+
+
+
+/*Part 1: Rajiv and Matthias----------------------------------------------------------------------------
+"Open and read each mark file field as a string, cast the ID and mark for each course in a separate
+array or vector."
+
+
+
+
+
+--------------------------------------------------------------------------------------------------------*/
+
+
+
+/*Part 2: Nathan ---------------------------------------------------------------------------------------
+"Calculate the average and GPA for each student using the mark arrays/vectors as input"
+
+
+
+
+
+
+
+--------------------------------------------------------------------------------------------------------*/
+
+
+
+/*Part 2: Alex ---------------------------------------------------------------------------------------
+ "Write the required information to a student summary file (as shown in assignment 2 file)"
+
+
+
+
+
+
+
+--------------------------------------------------------------------------------------------------------*/
+
 
 int main()
 {
